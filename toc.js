ae27eb709f4ffedf75e1e0ec05d87504734975c5
Gyula Nemeth <gyula.nemeth85@gmail.com>
2016-12-06 17:58:03 +0100

    init

---
+++ toc.js
@@ -0,0 +1,127 @@
+function toc(element) {
+	if (!element) {
+		return;
+	}
+
+	var elements;
+
+	if (element instanceof Array) {
+		elements = element;
+	} else {
+		elements = [element];
+	}
+
+	var hElements = [];
+	for (var idx = 0; idx < elements.length; idx += 1) {
+		var act = elements[idx];
+
+		var actHeadings = act.querySelectorAll("h1, h2, h3, h4, h5, h6");
+
+		for (var actIdx = 0; actIdx < actHeadings.length; actIdx += 1) {
+			hElements.push(actHeadings[actIdx]);
+		}
+	}
+
+	//check doc heading structure
+
+	if (!hElements.length) {
+		throw new Error("Heading elements were not found.");
+	}
+
+	if (hElements[0].tagName !== "H1") {
+		throw new Error("The first heading in the document should be an H1.");
+	}
+
+	var allowedTagNames = ["H1", "H2", "H3", "H4", "H5", "H6"];
+
+
+	var lastDepth = 0;
+	var root = {
+		element: hElements[0],
+		depth: 0,
+		children: []
+	};
+	var lastDescriptor = root;
+
+	for (var idx = 1; idx < hElements.length; idx += 1) {
+		var act = hElements[idx];
+
+		var actDepth = allowedTagNames.indexOf(act.tagName);
+
+		if (actDepth === 0) {
+			throw new Error("There should be only one H1 element in a HTML document.");
+		}
+
+		if (actDepth < 0) {
+			throw new Error("Invalid tagName", act);
+		}
+
+		var depthDiff = actDepth - lastDepth;
+
+		if (depthDiff > 1) {
+			throw new Error("The heading structure of your document contains an error. There should not be a " + act.tagName + " after " + hElements[idx - 1].tagName + ".", act);
+		}
+
+		var actDescriptor = {
+			element: act,
+			depth: actDepth,
+			children: []
+		};
+
+		setParent(actDescriptor);
+
+		lastDepth = actDepth;
+		lastDescriptor = actDescriptor;
+	}
+
+	function setParent(elementDescriptor) {
+		var parentDescriptor = lastDescriptor;
+
+		while (parentDescriptor.depth !== elementDescriptor.depth - 1) {
+			parentDescriptor = parentDescriptor.parent;
+		}
+
+		elementDescriptor.parent = parentDescriptor;
+		parentDescriptor.children.push(elementDescriptor);
+	}
+
+
+	function logTree(elementDescriptor) {
+		console.log(elementDescriptor.depth, elementDescriptor.element);
+
+		elementDescriptor.children.forEach(logTree);
+	}
+
+	//logTree(root);
+
+	var nextIdIdx = 0;
+	function createList(elementDescriptor) {
+		if (!elementDescriptor.element.id) {
+			elementDescriptor.element.id = "toc_heading_id_" + nextIdIdx;
+			nextIdIdx += 1;
+		}
+
+		var li = document.createElement("li");
+
+		var link = document.createElement("a");
+		link.href = "#" + elementDescriptor.element.id;
+		var text = document.createTextNode(elementDescriptor.element.innerText);
+		link.appendChild(text);
+
+		li.appendChild(link);
+
+		if (elementDescriptor.children.length > 0) {
+			var ul = document.createElement("ul");
+			li.appendChild(ul);
+			elementDescriptor.children.forEach(function(actChild) {
+				ul.appendChild(createList(actChild));
+			});
+		}
+
+		return li;
+	}
+
+	var tocUl = document.createElement("ul");
+	tocUl.appendChild(createList(root));
+	return tocUl;
+}
